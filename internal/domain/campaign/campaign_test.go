5de39b3f0cc8a0b369de8b1f2c1aa775f44904cd
pedropauloc <pedropaulohouston@gmail.com>
2024-09-08 18:09:49 +0100

    add campaign and tests

--- internal/domain/campaign/campaign_test.go
+++ internal/domain/campaign/campaign_test.go
@@ -1,1 +1,29 @@
 package campaign
+
+import (
+	"fmt"
+	"testing"
+)
+
+// é sempre bom testar o ponto de integração
+// como por exemplo quando eu cria uma nova campanha
+// testamos o comportamento
+
+func TestNewCampaign(t *testing.T) {
+	name := "Campaign X"
+	content := "Body"
+	contacts := []string{"email1@gmail.com", "email2@gmail.com"}
+
+	campaign := NewCampaign(name, content, contacts)
+	fmt.Println(campaign)
+	
+	if campaign.ID != "1" {
+		t.Errorf("expected 1")
+	} else if campaign.Name != name {
+		t.Errorf("expected correct name")
+	} else if campaign.Content != content {
+		t.Errorf("expected correct content")
+	} else if len(campaign.Contacts) != len(contacts) {
+		t.Errorf("expected correct contacts")
+	}
+}
